26e7d3bc1510ca2e592df0554bb25114c3bbdca5
mamun-prodhan <mamunpr9@gmail.com>
2023-08-20 22:23:47 +0600

    total price calculated

--- js/calculation.js
+++ js/calculation.js
@@ -13,7 +13,28 @@ function clickhandler(target){
     const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
     total = (parseFloat(total) + parseFloat(price)).toFixed(2);
     document.getElementById('total-price').innerText = total;
+    document.getElementById('total-amount').innerText = total;
 
+    // purchase button enable
+    const purchaseButton = document.getElementById('btn-purchase');
+    if(total > 0){
+        purchaseButton.removeAttribute('disabled');
+        purchaseButton.classList.add('bg-[#E527B2]');
+        purchaseButton.classList.add('hover:bg-[#ca0094]');
+        purchaseButton.classList.remove('bg-[#ff94e3]');
+    }
+
+    // Apply button enable
+    const applyButton = document.getElementById('btn-apply');
+    if(total >= 200){
+        applyButton.removeAttribute('disabled');
+        applyButton.classList.add('bg-[#E527B2]');
+        applyButton.classList.add('hover:bg-[#ca0094]');
+        applyButton.classList.remove('bg-[#ff94e3]');
+    }
+}
+
+function discountHandler(){
 
 }
 
