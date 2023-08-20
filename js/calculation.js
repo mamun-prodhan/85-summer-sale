b7fc558d6d3dd905bf9756f50dfc2b101c644ab5
mamun-prodhan <mamunpr9@gmail.com>
2023-08-20 11:38:40 +0600

    card design completed

---
+++ js/calculation.js
@@ -0,0 +1,13 @@
+function clickhandler(target){
+    console.log(target.childNodes[3].childNodes[3].innerText);
+    console.log(target.childNodes[3].childNodes[5].innerText);
+
+}
+
+
+
+
+
+
+
+    // console.log(parseFloat(taka).toFixed(2))
