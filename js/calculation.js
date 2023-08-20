a7113195e0ee863c44a1bacd6bc0dd30a7e4097c
mamun-prodhan <mamunpr9@gmail.com>
2023-08-20 15:28:27 +0600

    total price added

--- js/calculation.js
+++ js/calculation.js
@@ -1,6 +1,19 @@
+let total = 0;
+
 function clickhandler(target){
-    console.log(target.childNodes[3].childNodes[3].innerText);
-    console.log(target.childNodes[3].childNodes[5].innerText.split(" ")[0]);
+
+    // select item name and append in li
+    const addedItemContainer = document.getElementById('selected-item');
+    const itemName = target.childNodes[3].childNodes[3].innerText;
+    const li = document.createElement('li');
+    li.innerText = itemName;
+    addedItemContainer.appendChild(li);
+    
+    // price calculation
+    const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    total = (parseFloat(total) + parseFloat(price)).toFixed(2);
+    document.getElementById('total-price').innerText = total;
+
 
 }
 
