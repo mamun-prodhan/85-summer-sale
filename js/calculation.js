66679eac705c6884ca3fd450964394b2316fbbd3
mamun-prodhan <mamunpr9@gmail.com>
2023-08-20 12:59:04 +0600

    responsive card section completed

--- js/calculation.js
+++ js/calculation.js
@@ -1,6 +1,6 @@
 function clickhandler(target){
     console.log(target.childNodes[3].childNodes[3].innerText);
-    console.log(target.childNodes[3].childNodes[5].innerText);
+    console.log(target.childNodes[3].childNodes[5].innerText.split(" ")[0]);
 
 }
 
